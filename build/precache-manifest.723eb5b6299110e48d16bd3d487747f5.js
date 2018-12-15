bbac58880d5a5bee8de7d5c4dc47171783b0ba42
Rivo Laks <code@rivolaks.com>
2018-12-15 13:06:29 +0100

    Rebuild, add plotly dep

---
+++ build/precache-manifest.723eb5b6299110e48d16bd3d487747f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a39b924ef39ec687f2227c5b8cbf487c",
+    "url": "/static/media/BeeSpotLogo.a39b924e.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "577f409efaf21108cfe5",
+    "url": "/static/js/main.577f409e.chunk.js"
+  },
+  {
+    "revision": "563b2de2cd56e388eee3",
+    "url": "/static/js/1.563b2de2.chunk.js"
+  },
+  {
+    "revision": "577f409efaf21108cfe5",
+    "url": "/static/css/main.31917586.chunk.css"
+  },
+  {
+    "revision": "563b2de2cd56e388eee3",
+    "url": "/static/css/1.10801a8d.chunk.css"
+  },
+  {
+    "revision": "9cabccb686d7c9ed4bae36df1d7630fa",
+    "url": "/index.html"
+  }
+];
