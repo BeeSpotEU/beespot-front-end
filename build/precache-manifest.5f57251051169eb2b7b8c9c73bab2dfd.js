8fa49d26859904fcc123041b8ab96e07bb0afbdc
Pim van Gennip <pim@iconize.nl>
2018-12-15 12:10:25 +0100

    added build

---
+++ build/precache-manifest.5f57251051169eb2b7b8c9c73bab2dfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a39b924ef39ec687f2227c5b8cbf487c",
+    "url": "/static/media/BeeSpotLogo.a39b924e.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f117de5228fbc2ce1912",
+    "url": "/static/js/main.f117de52.chunk.js"
+  },
+  {
+    "revision": "4b48d937a48321edda51",
+    "url": "/static/js/1.4b48d937.chunk.js"
+  },
+  {
+    "revision": "f117de5228fbc2ce1912",
+    "url": "/static/css/main.d28c1ed2.chunk.css"
+  },
+  {
+    "revision": "4b48d937a48321edda51",
+    "url": "/static/css/1.d8637455.chunk.css"
+  },
+  {
+    "revision": "9a4d52c6e91565264195723637192256",
+    "url": "/index.html"
+  }
+];
